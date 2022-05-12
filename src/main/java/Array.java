03a24e67a8eff5dc16b3534a1365a34057ba6c90
Betty-Oforiwa <maameakuaoforiwa@gmail.com>
2022-05-12 16:37:15 +0000

    array data structures added

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -2,24 +2,25 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Array {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 //        One Dimensional Array
-        String[] joe = {"Ella","cake","parry"};
+        String[] joe = {"Ella", "cake", "parry"};
         System.out.println(joe[0]);
         joe[0] = "Audrey";
         System.out.println(joe[0]);
-         for (int i = 0; i <joe.length; i++){
-             System.out.println(joe[i]);
-         }
+        for (int i = 0; i < joe.length; i++) {
+            System.out.println(joe[i]);
+        }
+
 
 //         two Dimensional Array
-        int[][] bet = {{1,4,2},{5,2,5}};
+        int[][] bet = {{1, 4, 2}, {5, 2, 5}};
         System.out.println(bet[0][0]);
         bet[0][2] = 6;
         System.out.println(bet[0][2]);
         System.out.println(bet[1][2]);
-        for(int i = 0; i<bet.length; i++) {
-            for(int b = 0; b<bet[i].length; b++){
+        for (int i = 0; i < bet.length; i++) {
+            for (int b = 0; b < bet[i].length; b++) {
                 System.out.println("---------");
                 System.out.println(bet[i][b]);
             }
@@ -27,24 +28,23 @@ public class Array {
         System.out.println(bet.length);
 
 
-
-        int numbers[] ={1,2,3,4,5,6,7,8,9,10};
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int[] arr = new int[10];
         int value = 10;
-         for(int b = 0; b < arr.length; b++){
-             arr[b] = value;
-             value += 10;
-
-         }
+        for (int b = 0; b < arr.length; b++) {
+            arr[b] = value;
+            value += 10;
+        }
         System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(numbers));
 
 
-        //Take 10 integer inputs from user and store them in an array and print them on screen.
-        int[] a = new int[10];
+//        Take 10 integer inputs from user and store them in an array and print them on screen.
+        int[] a = new int[5];
         Scanner s = new Scanner(System.in);
         System.out.println("enter number :");
-        for(int i= 0; i< a.length; i++){
-             a[i] = s.nextInt();
+        for (int i = 0; i < a.length; i++) {
+            a[i] = s.nextInt();
 
         }
         System.out.println(Arrays.toString(a));
@@ -52,11 +52,10 @@ public class Array {
 
 //        Take 10 integer inputs from user and store them in an array. Again ask user to give a number.
 //        Now, tell user whether that number is present in array or not.
-
-        int[] b = new int[10];
+        int[] b = new int[5];
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter 10 digits :");
-        for(int i= 0; i< b.length; i++) {
+        for (int i = 0; i < b.length; i++) {
             b[i] = sc.nextInt();
         }
         System.out.println(Arrays.toString(b));
@@ -64,55 +63,96 @@ public class Array {
         Scanner sn = new Scanner(System.in);
         int ui = sn.nextInt();
         boolean numexist = false;
-        for(int v : b){
-            if(v == ui){
+        for (int v : b) {
+                if (v == ui) {
                 numexist = true;
             }
         }
-        if(numexist){
+        if (numexist) {
             System.out.println("number exist");
-        }
-        else{
+        } else {
             System.out.println("Sorry the number doesn't exist");
         }
 
+
 //        Take 20 integer inputs from user and print the following:
-//        number of positive numbers
-//        number of negative numbers
-//        number of odd numbers
-//        number of even numbers
-//        number of 0s.
-
-        int[] wholeNumbers = new int [5];
+//        number of positive numbers,number of negative numbers,number of odd numbers
+//        number of even numbers and number of 0s.
+        int[] wholeNumbers = new int[5];
         Scanner zn = new Scanner(System.in);
-        System.out.println(" enter 5 numbers : ");
-        for(int i = 0; i<wholeNumbers.length; i++){
-             wholeNumbers[i] = zn.nextInt();
+        int pos = 0;
+        int neg = 0;
+        int zero = 0;
+        int even = 0;
+        int odd = 0;
+        System.out.println("enter numbers :");
+        for (int i = 0; i < wholeNumbers.length; i++) {
+            wholeNumbers[i] = zn.nextInt();
+            if (wholeNumbers[i] > 0) {
+                pos++;
+            } else if (wholeNumbers[i] < 0) {
+                neg++;
+            } else {
+                zero++;
+            }
+            if (wholeNumbers[i] % 2 != 0) {
+                odd++;
+            } else {
+                even++;
+            }
         }
-        System.out.println(Arrays.toString(wholeNumbers));
-        for(int i : wholeNumbers){
-            if(i > 0){
-                System.out.println("positive numbers : " + i);
-            }
-            if(i < 0){
-                System.out.println("negative numbers : " + i);
-            }
-            if(i % 2 != 0){
-                System.out.println("odd numbers :" + i);
-            }
-            if(i % 2 == 0){
-                System.out.println("even numbers : " + i);
-            }
-            if(i == 0) {
-                System.out.println("zero numbers : " + i );
+        System.out.println("posNum:" + pos + "\nnegNum:" + neg + "\nzeroNum:" + zero + "\noddNum:" + odd + "\nevenNum:" + even);
 
-            }
 
+//        Take 5 integer inputs from user and store them in an array.
+//        Now, copy all the elements in an array but in reverse order.
+        int[] yaw = new int[5];
+        int temp = 0;
+        Scanner y = new Scanner(System.in);
+        System.out.println("Please enter 5 number:");
+        for (int i = 0; i < yaw.length; i++) {
+            yaw[i] = y.nextInt();
         }
+        System.out.println(Arrays.toString(yaw));
+        for (int i = 0; i < yaw.length; i++)
+            for (int j = i + 1; j < yaw.length; j++) {
+                if (yaw[i] < yaw[j]) {
+                    temp = yaw[i];
+                    yaw[i] = yaw[j];
+                    yaw[j] = temp;
+                }
+            }
+        System.out.println(Arrays.toString(yaw));
+
+
+//        Write a program to find the sum and product of all elements of an array.
+        int[] array = {1, 2, 3, 5, 7};
+        int sum = 0;
+        int product = 1;
+        for (int i = 0; i < array.length; ++i) {
+            System.out.println(sum += array[i]);
+        }
+        System.out.println("------------------------");
+
+        for (int i = 0; i < array.length; ++i) {
+            System.out.println(product *= array[i]);
+
+            //Initialize and print all elements of a 2D array.
+            int[][] twoD = {{1, 2, 3}, {4, 6, 7, 8}};
+            System.out.println();
+            for (int d = 0; d < twoD.length; d++)
+                for (int c = 0; c < twoD[d].length; c++) {
+                    System.out.println(twoD[d][c]);
+                }
+        }
+
+
+//        Find largest and smallest elements of an array.
 
     }
+}
 
 
 
-    }
+
 

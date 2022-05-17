4f63289def985ecf6519987194a6a93b14503e39
Betty-Oforiwa <maameakuaoforiwa@gmail.com>
2022-05-17 13:52:22 +0000

    Merge remote-tracking branch 'origin/array_data_sturcture' into array_data_sturcture
    # Conflicts:
    #	src/main/java/Array.java

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -1,44 +1,45 @@
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Array {
     public static void main(String[] args) {
-//        One Dimensional Array
-        String[] joe = {"Ella", "cake", "parry"};
-        System.out.println(joe[0]);
-        joe[0] = "Audrey";
-        System.out.println(joe[0]);
-        for (int i = 0; i < joe.length; i++) {
-            System.out.println(joe[i]);
-        }
+////        One Dimensional Array
+//        String[] joe = {"Ella", "cake", "parry"};
+//        System.out.println(joe[0]);
+//        joe[0] = "Audrey";
+//        System.out.println(joe[0]);
+//        for (int i = 0; i < joe.length; i++) {
+//            System.out.println(joe[i]);
+//        }
+//
+////         two Dimensional Array
+//        int[][] bet = {{1, 4, 2}, {5, 2, 5}};
+//        System.out.println(bet[0][0]);
+//        bet[0][2] = 6;
+//        System.out.println(bet[0][2]);
+//        System.out.println(bet[1][2]);
+//        for (int i = 0; i < bet.length; i++) {
+//            for (int b = 0; b < bet[i].length; b++) {
+//                System.out.println("---------");
+//                System.out.println(bet[i][b]);
+//            }
+//        }
+//        System.out.println(bet.length);
+//
+//
+//        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//        int[] arr = new int[10];
+//        int value = 10;
+//        for (int b = 0; b < arr.length; b++) {
+//            arr[b] = value;
+//            value += 10;
+//        }
+//        System.out.println(Arrays.toString(arr));
+//        System.out.println(Arrays.toString(numbers));
 
-//         two Dimensional Array
-        int[][] bet = {{1, 4, 2}, {5, 2, 5}};
-        System.out.println(bet[0][0]);
-        bet[0][2] = 6;
-        System.out.println(bet[0][2]);
-        System.out.println(bet[1][2]);
-        for (int i = 0; i < bet.length; i++) {
-            for (int b = 0; b < bet[i].length; b++) {
-                System.out.println("---------");
-                System.out.println(bet[i][b]);
-            }
-        }
-        System.out.println(bet.length);
 
-
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int[] arr = new int[10];
-        int value = 10;
-        for (int b = 0; b < arr.length; b++) {
-            arr[b] = value;
-            value += 10;
-        }
-        System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(numbers));
-
-
-//        Take 10 integer inputs from user and store them in an array and print them on screen.
+//        Take 5 integer inputs from user and store them in an array and print them on screen.
         int[] a = new int[5];
         Scanner s = new Scanner(System.in);
         System.out.println("enter number :");
@@ -63,7 +64,7 @@ public class Array {
         int ui = sn.nextInt();
         boolean numexist = false;
         for (int v : b) {
-                if (v == ui) {
+            if (v == ui) {
                 numexist = true;
             }
         }
@@ -136,6 +137,7 @@ public class Array {
         for (int i = 0; i < array.length; ++i) {
             System.out.println(product *= array[i]);
 
+
             //Initialize and print all elements of a 2D array.
             int[][] twoD = {{1, 2, 3}, {4, 6, 7, 8}};
             System.out.println();
@@ -144,9 +146,25 @@ public class Array {
                     System.out.println(twoD[d][c]);
                 }
         }
+        System.out.println("---------------------------------------");
 
 
 //        Find largest and smallest elements of an array.
+        int[] x = {1, 2, 3, 4, 5};
+        int max = x[0];
+        int min = x[0];
+        for (int i : x ) {
+            if (max <  i) {
+                max = i;
+            }
+            if (min > i) {
+                min = i;
+            }
+        }
+        System.out.println(max);
+        System.out.println(min);
+
+
 
     }
 }
